6c4c4993f705eede24950ef5d896d24099d81213
vivek shrivastwa <vinodkumarshrivastawa@gmail.com>
2021-05-28 23:42:44 +0530

    Initial Commit

---
+++ I_am_rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I_am_rich
+//
+//  Created by vivek shrivastwa on 28/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
